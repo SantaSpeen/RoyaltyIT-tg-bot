07d4be52f69d4a66ce2f494fef0c3a036aec3e01
SantaSpeen <admin@ahegao.ovh>
2022-03-21 17:30:29 +0300

    Small refactor

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import logging
 
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
+from aiogram.types import ParseMode
 from aiogram.utils import executor
 
 from config import Config
@@ -17,26 +18,20 @@ tools = Tools(config, dp)
 
 @dp.message_handler(commands=['admins'])
 async def bot_admins(msg: types.Message):
-    message = "%(owner)-s"
+    message = "Администраторы нашего сообщества:\n%(owner)-s"
     admin_list = await bot.get_chat_administrators(config.remote_chat)
     i = 1
     for admin_object in admin_list:
         user = admin_object['user']
-        status = admin_object['status'].replace("administrator", "Администратор").replace("creator", "Создатель")
+        status = admin_object['status'].replace("administrator", "Администратор").replace("creator", "Создатель:")
         if status == "Администратор":
             if not user['is_bot']:
-                message += f"{i}. {status:13} @{user['username']}\n"
+                message += f"`{i}. {status+':'}` `@{user['username']}`\n"
                 i += 1
         else:
-            message %= {"owner": f"0. {status:13} @{user['username']}\n"}
+            message %= {"owner": f"`0. {status:14}` `@{user['username']}`\n"}
 
-    await msg.reply(message, disable_notification=True)
-
-
-# @dp.message_handler(content_types=['text'])
-# async def msg_logger(msg: types.Message):
-#     log.info(f"New message from {msg.from_user.id}({msg.from_user.username}) in {msg.chat.id}: '{msg.text}'")
-
+    await msg.reply(message, parse_mode=ParseMode.MARKDOWN)
 
 tools.bind_static_messages()
 
